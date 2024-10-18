da6e08c3e5cadc57f17bab02a1474c679d630db6
IniOluwa Longe <inilonge@gmail.com>
2024-10-18 02:30:10 +0100

    Changes

--- lib/screens/home/men/home_page_men.dart
+++ lib/screens/home/men/home_page_men.dart
@@ -1,4 +1,4 @@
-import 'package:cloture/screens/home/men/categories.dart';
+import 'package:cloture/screens/home/men/categories_men.dart';
 import 'package:cloture/utilities/shimmers.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
